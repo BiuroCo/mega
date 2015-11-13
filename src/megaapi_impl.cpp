f94521bc7cc920ceacbd34fe84b8c916d3c0968d
Sergio Hernández <shs@mega.co.nz>
2015-11-13 18:38:05 +0100

    Remove the ability to set `authring` and `lstint`
    Apps mustn't modify the value of those attributes, but just query them.

--- src/megaapi_impl.cpp
+++ src/megaapi_impl.cpp
@@ -8726,20 +8726,7 @@ void MegaApiImpl::sendPendingRequests()
                     attrvalue = value;
                     break;
                 }
-
-                case MegaApi::USER_ATTR_AUTHRING:
-                {
-                    attrname = "*!authring";
-                    attrvalue = value;
-                    break;
-                }
-
-                case MegaApi::USER_ATTR_LAST_INTERACTION:
-                {
-                    attrname = "*!lstint";
-                    attrvalue = value;
-                    break;
-                }
+                // authentication ring and last interaction are ro attributes
 
                 default:
                 {
